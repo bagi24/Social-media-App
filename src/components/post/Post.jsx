c1335c883938e38d9ae72583622c057b621cf9c7
unknown <bagrat.injgia24@gmail.com>
2023-10-27 21:04:10 +0400

    create UsersData Component and add Data dynamically

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,7 +1,13 @@
 import "./post.css"
 import {MoreVert} from '@mui/icons-material';
+import {Users} from "../../UsersData"
 
-export default function Post() {
+export default function Post({post}) {
+  // console.log(post)
+
+  const user =Users.filter(u =>u.id ===1)
+  console.log(user)
+    
   return (
     <div className="post">
   <div className="postWrapper">
@@ -9,9 +15,9 @@ export default function Post() {
     <div className="postTop">
 <div className="postTopLeft">
 
-    <img  className="postProfileImg" src="/assets/persons/1.jpeg" alt="" />
-<span className="postUsername"> Bagrat Injgia</span>
-<span className="postDate"> 5 mins ago</span>
+    <img  className="postProfileImg" src={Users.filter(u => u.id === post.userId)[0].profilePicture} alt="" />
+<span className="postUsername">{Users.filter(u => u.id === post.userId)[0].username}</span>
+<span className="postDate"> {post.date}</span>
 
 </div>
 <div className="postTopRight">
@@ -22,8 +28,8 @@ export default function Post() {
 
     </div>
     <div className="postCenter">
-<span className="postText"> Hey! its my first Post : </span>
-<img  className="postImg" src="/assets/posts/1.jpeg " alt="" />
+<span className="postText"> {post?.desc} </span>
+<img  className="postImg" src={post.photo} alt="" />
 
 
 
@@ -33,11 +39,11 @@ export default function Post() {
         <div className="postBottomLeft">
             <img className="likeIcon" src="/assets/like.png" alt="" />
             <img className="likeIcon" src="/assets/heart.png" alt="" />
-     <span className="postLikeCounter">32 people like it </span>
+     <span className="postLikeCounter">{post.like} people like it </span>
         </div>
         <div className="postBottomRight">
 
-            <span className="postCommentText"> 9 comments</span>
+            <span className="postCommentText"> {post.comment} comments</span>
         </div>
     </div>
   </div>
