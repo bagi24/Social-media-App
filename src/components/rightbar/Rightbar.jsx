9939a58a3af15884bbe44e1dc744b5c3342adc35
unknown <bagrat.injgia24@gmail.com>
2023-10-27 16:22:40 +0400

    add rightbar Component

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,99 @@ import "./rightbar.css";
 export default function Rightbar() {
   return (
     <div className="rightbar">
-      rightbar
+   <div className="rightbarWrapper">
+    <div className="birthdayConteiner">
+      <img className="birthdayImg" src="/assets/gift.png" alt="" />
+   <span className="birthdatText"> <b> Davit injgia </b>  and  <b> 3 other Friends</b> have a birthday today </span>
+    </div>
+
+
+    <img className="rightbarAd" src="/assets/ad.png" alt="" />
+    <h4 className="rightbarTitle"> Online Friends</h4>
+    <ul className="rightbarFriendList">
+      <li className="rightbarFriend">
+
+        <div className="rightbarProfileImgConteiner">
+          <img className="rightbarProfileImg" src="/assets/persons/3.jpeg" alt="" />
+      
+      <span className="rightbarOnline"></span>
+        </div>
+<span className="rightbarUsername"> Patrisia Injgia </span>
+      </li>
+        <li className="rightbarFriend">
+
+        <div className="rightbarProfileImgConteiner">
+          <img className="rightbarProfileImg" src="/assets/persons/3.jpeg" alt="" />
+      
+      <span className="rightbarOnline"></span>
+        </div>
+<span className="rightbarUsername"> Patrisia Injgia </span>
+      </li>
+        <li className="rightbarFriend">
+
+        <div className="rightbarProfileImgConteiner">
+          <img className="rightbarProfileImg" src="/assets/persons/3.jpeg" alt="" />
+      
+      <span className="rightbarOnline"></span>
+        </div>
+<span className="rightbarUsername"> Patrisia Injgia </span>
+      </li>
+        <li className="rightbarFriend">
+
+        <div className="rightbarProfileImgConteiner">
+          <img className="rightbarProfileImg" src="/assets/persons/3.jpeg" alt="" />
+      
+      <span className="rightbarOnline"></span>
+        </div>
+<span className="rightbarUsername"> Patrisia Injgia </span>
+      </li>
+        <li className="rightbarFriend">
+
+        <div className="rightbarProfileImgConteiner">
+          <img className="rightbarProfileImg" src="/assets/persons/3.jpeg" alt="" />
+      
+      <span className="rightbarOnline"></span>
+        </div>
+<span className="rightbarUsername"> Patrisia Injgia </span>
+      </li>
+        <li className="rightbarFriend">
+
+        <div className="rightbarProfileImgConteiner">
+          <img className="rightbarProfileImg" src="/assets/persons/3.jpeg" alt="" />
+      
+      <span className="rightbarOnline"></span>
+        </div>
+<span className="rightbarUsername"> Patrisia Injgia </span>
+      </li>
+        <li className="rightbarFriend">
+
+        <div className="rightbarProfileImgConteiner">
+          <img className="rightbarProfileImg" src="/assets/persons/3.jpeg" alt="" />
+      
+      <span className="rightbarOnline"></span>
+        </div>
+<span className="rightbarUsername"> Patrisia Injgia </span>
+      </li>
+        <li className="rightbarFriend">
+
+        <div className="rightbarProfileImgConteiner">
+          <img className="rightbarProfileImg" src="/assets/persons/3.jpeg" alt="" />
+      
+      <span className="rightbarOnline"></span>
+        </div>
+<span className="rightbarUsername"> Patrisia Injgia </span>
+      </li>
+        <li className="rightbarFriend">
+
+        <div className="rightbarProfileImgConteiner">
+          <img className="rightbarProfileImg" src="/assets/persons/3.jpeg" alt="" />
+      
+      <span className="rightbarOnline"></span>
+        </div>
+<span className="rightbarUsername"> Patrisia Injgia </span>
+      </li>
+    </ul>
+   </div>
     </div>
   )
 }
